0b3e5ba9de634287c75357ae23956af1d8868437
thieproc2002 <trantrongthiep2002@gmail.com>
2024-05-24 10:40:56 +0700

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,17 @@
+const images = {
+    logo: require('~/assets/images/logo.png'),
+    avt: require('~/assets/images/no-image.png'),
+    noImg: require('~/assets/images/no-image.png'),
+    listfriend: require('~/assets/images/listfriend.jpg'),
+    groupchat: require('~/assets/images/groupchat.png'),
+    // background: require('`~/assets/images/background.png'),
+    blankIcon: require('~/assets/images/icons/blank.png'),
+    excelIcon: require('~/assets/images/icons/excel-icon.png'),
+    pdfIcon: require('~/assets/images/icons/pdf-icon.png'),
+    pwpIcon: require('~/assets/images/icons/pwp-icon.png'),
+    wordIcon: require('~/assets/images/icons/word-icon.png'),
+    txtIcon: require('~/assets/images/icons/txt-icon.png'),
+    preLoadingMessage: require('~/assets/images/gif/loading-messages.gif'),
+};
+
+export default images;
